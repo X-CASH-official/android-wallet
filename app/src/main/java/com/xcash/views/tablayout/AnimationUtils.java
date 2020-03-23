7af85482d3eb076f727115ba5292d089a0477aec
plbgnt <paul.bugnot@gmail.com>
2020-03-23 19:13:51 +0100

    chore: update snakeway copyright

--- app/src/main/java/com/xcash/views/tablayout/AnimationUtils.java
+++ app/src/main/java/com/xcash/views/tablayout/AnimationUtils.java
@@ -1,7 +1,6 @@
-/*
+ /*
+ * Copyright (C) 2019-2020 The X-Cash Foundation
  * Copyright (C) 2015 The Android Open Source Project
- *
- * Copyright (c) 2019 by snakeway
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
