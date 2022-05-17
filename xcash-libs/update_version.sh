6a093686b0e0245dd1dd8157dc4fe01f07002447
Francisco M <fr4nciscom@protonmail.com>
2022-05-17 23:36:14 +0100

    Updated gradle config to generate xml with xcash-core version

---
+++ xcash-libs/update_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+LIB_DIR=xcash-libs/monero/lib/arm64-v8a
+LIB_FILE=libversion.a
+
+current_path=`pwd`
+
+VERSION=`strings $LIB_DIR/$LIB_FILE | grep -P [0-9]+\.[0-9]+\.[0-9]+-release | tr -d '\-
+release'`
+
+sed "s/%VERSION%/$VERSION/g" xcash-libs/version_template.xml > app/src/main/res/xml/version.xml
+
+cd $current_path
